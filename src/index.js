7b48de59e89d4e2f14150899800b71711e24a1f5
xuzhida <xuzhida@163.com>
2021-07-02 15:50:01 +0800

    init

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ const install = function(Vue) {
   });
 };
 
+if (typeof window !== 'undefined' && window.Vue) {
+  console.log("Comp-load")
+  // Comp.install(window.Vue)
+  window.Vue.use(install);
+  // window.Vue.use(Comp.install);
+}
+
 export default {
   version: '1.0.0',
   total: components.length,
   install,
-  Button,
 };
 
 // 单个组件可以直接这样导出
